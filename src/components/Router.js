db8d0916558d6fbbf773c52e9a1bf3fe75796ae8
Vladimir Tesiolchin <vladimir@MacBook-Pro-Vladimir.local>
2021-01-26 09:01:43 -0800

    Create router

---
+++ src/components/Router.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import Main from '../Pages/Main';
+import Category from '../Pages/Category';
+import Recipe from '../Pages/Recipe';
+
+function Router() {
+  return (
+    <React.Fragment>
+      <Router>
+      <Switch>
+        <Route exact path='/' component={Main}/>
+        <Route path="/category" component={Category} />
+        <Route path="/recipe" component={Recipe} />
+      </Switch>
+      </Router>
+    </React.Fragment>
+  );
+}
+
+export default Router;
