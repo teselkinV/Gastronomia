750741f85e29b747e0cb0cf7629619188153d932
Vladimir Tesiolchin <vladimir@MacBook-Pro-Vladimir.local>
2021-01-26 09:33:16 -0800

    Created dark mode function

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import Darkmode from './Darkmode/Darkmode';
+
 function Navbar() {
     return (
         <div className='navbar'>
@@ -8,6 +10,7 @@ function Navbar() {
                 <li className='link'><Link to='/'>Main</Link></li>
                 <li className='link'><Link to='/category'>Category</Link></li>
                 <li className='link'><Link to='/recipe'>Recipe</Link></li>
+                <li className='link'><Darkmode /></li>
             </ul>
         </div>
     );
