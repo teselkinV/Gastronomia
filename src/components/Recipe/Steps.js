2fc9089bd952c315eda7d6b631fd065f175e8c4b
Vladimir Tesiolchin <vladimir@MacBook-Pro-Vladimir.local>
2021-02-09 10:51:40 -0800

    Changed list of steps

--- src/components/Recipe/Steps.js
+++ src/components/Recipe/Steps.js
@@ -16,14 +16,12 @@ function Steps({ingridients, steps, title, time}) {
            <div>{timeStep()}</div>
             <ul>
                 {ingridients.map((amount) => (
-                    <>
-                    <li>{Object.entries(amount)}</li>
-                    </>
+                    <li><span style={{fontWeight: 'bold'}}>{Object.keys(amount)} </span>{Object.values(amount)}</li>
                 ))}
             </ul>
             <ol>
-                {steps.map((num, st) => (
-                    <li><span style={{fontSize: '20px'}}>{Object.keys(num)} </span>{Object.values(num)}</li>
+                {steps.map((num) => (
+                    <li><span className='steps-span'>{Object.keys(num)}. </span>{Object.values(num)}</li>
                 ))}
             </ol>
         </div>
