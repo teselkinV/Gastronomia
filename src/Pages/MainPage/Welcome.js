6a7ed35b0acff513f827235d153122b92cb01fa8
Vladimir Tesiolchin <vladimir@MacBook-Pro-Vladimir.local>
2021-02-24 09:37:14 -0800

    Build main page with new components

---
+++ src/Pages/MainPage/Welcome.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Welcome() {
+    return (
+        <div style={{textAlign: "center"}}>
+            <h3>image</h3>
+            <div>
+                <h2>Welcome title</h2>
+                <p>Welcome text</p>
+                <button>Learn more</button>
+            </div>
+            
+        </div>
+    );
+}
+
+export default Welcome;
