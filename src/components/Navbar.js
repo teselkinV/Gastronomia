6aa5277bd1755633afe810bd80fddc7e406e19dc
Vladimir Tesiolchin <vladimir@MacBook-Pro-Vladimir.local>
2021-02-26 10:18:52 -0800

    Added search

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,6 +6,9 @@ import { Text } from './Language/Language';
 import Darkmode from './Darkmode/Darkmode';
 import Selector from '../components/Language/Selector';
 
+// Search
+import Search from '../components/Search/Search';
+
 const Navbar = () => {
     return (
         <div className='navbar'>
@@ -14,7 +17,8 @@ const Navbar = () => {
                 <li className='link'><Link to='/about'><Text t="about" /></Link></li>
                 <li className='link'><Link to='/recipes'><Text t="recipes" /></Link></li>
                 <li className='link'><Darkmode /></li>
-                <li><Selector /></li>
+                <li className='link'><Selector /></li>
+                <li className='link'><Search /></li>
             </ul>
         </div>
     );
---
+++ src/components/Search/Search.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Search() {
+    return (
+        <div>
+            <form>
+                <input 
+                    type='text'
+                    name='search'
+                    placeholder='search'
+                    />
+            </form>
+        </div>
+    );
+}
+
+export default Search;
