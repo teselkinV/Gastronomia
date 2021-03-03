e16f5ef2a9af24bab6ada184adfec24331462431
Vladimir Tesiolchin <vladimir@MacBook-Pro-Vladimir.local>
2021-03-03 10:33:03 -0800

    Fixed router issue

--- src/components/Recipe/Card.js
+++ src/components/Recipe/Card.js
@@ -4,9 +4,9 @@ import { Link } from 'react-router-dom';
 function Card({id, title}) {
     return (
         <div className='recipe'>
-            {/* <Link to={`/recipes/${id}`}> */}
+            <Link to={`/recipes/${id}`}>
             <h2>{title}</h2>
-            {/* </Link> */}
+            </Link>
         </div>
     );
 }
