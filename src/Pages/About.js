e8e3f5fbc3ed3826cab2a60478d5cfc390fef3ba
Vladimir Tesiolchin <vladimir@MacBook-Pro-Vladimir.local>
2021-03-17 09:42:32 -0700

    Added About page

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -2,12 +2,21 @@ import React from 'react';
 import { Text } from '../components/Language/Language';
 
 import Navbar from '../components/Navbar';
+import FavoriteRecipes from '../components/CardsWithRecipe/FavoriteRecipes';
+import BreadCollection from '../components/CollectionsOfRecipes/BreadCollection';
+import Footer from '../components/Footer/Footer';
 
 function About() {
     return (
         <div>
             <Navbar />
             <h1><Text t="about-us" /></h1>
+            <h3>Banner</h3>
+            <h4>My photo</h4>
+            <p>Some text about me and why I do this.</p>
+            <FavoriteRecipes />
+            <BreadCollection />
+            <Footer />
         </div>
     );
 }
