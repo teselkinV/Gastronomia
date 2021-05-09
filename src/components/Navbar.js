1c3bc51bac92ecc42ea042fb2c324c0e42462e04
Vladimir Tesiolchin <vladimir@MacBook-Pro-Vladimir.local>
2021-05-09 10:39:31 -0700

    Changed navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,30 +1,46 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { Text } from './Language/Language';
-
+import React from "react";
+import { Link } from "react-router-dom";
+import { Text } from "./Language/Language";
 
 // Modes components
-import Darkmode from './Darkmode/Darkmode';
-import Selector from '../components/Language/Selector';
+import Darkmode from "./Darkmode/Darkmode";
+import Selector from "../components/Language/Selector";
 
 // Search
-import Search from '../components/Search/Search';
+import Search from "../components/Search/Search";
 
 const Navbar = () => {
-    return (
-        <div className='navbar'>
-            <ul className='navbar-list'>
-                <li>logo</li>
-                <li className='link'><Link to='/'><Text t="main" /></Link></li>
-                <li className='link'><Link to='/about'><Text t="about" /></Link></li>
-                <li className='link'><Link to='/category'><Text t="category" /></Link></li>
-                <li className='link'><Link to='/contacts'><Text t="contacts" /></Link></li>
-                <li className='link'><Selector /></li>
-                <li className='link'><Darkmode /></li>
-                <li className='link'><Search /></li>
-            </ul>
-        </div>
-    );
-}
+  return (
+    <div className="navbar">
+      <ul className="navbar-list">
+        <li>logo</li>
+        <li className="link">
+          <Link to="/about">
+            <Text t="about" />
+          </Link>
+        </li>
+        <li className="link">
+          <Link to="/category">
+            <Text t="category" />
+          </Link>
+        </li>
+        <li className="link">
+          <Link to="/contacts">
+            <Text t="contacts" />
+          </Link>
+        </li>
+        <li className="link">
+          <Selector />
+        </li>
+        <li className="link">
+          <Darkmode />
+        </li>
+        <li className="link">
+          <Search />
+        </li>
+      </ul>
+    </div>
+  );
+};
 
 export default Navbar;
